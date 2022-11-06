f8e4434d4d804b58e20aca3c5b9902a785d4faf6
VitaBrrevis <vitalina.doronina@gmail.com>
2022-11-06 20:41:09 +0200

    12 line task

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");//first dublicate
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");//second dublicate
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");//third dublicate
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");//fourth dublicate
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");//fifth dublicate
